20250724ef168182b93bb14e380a66e6048c3b10
mauroporras <167700+mauroporras@users.noreply.github.com>
2021-11-05 15:46:08 -0500

    WIP: main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,67 +1,61 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react'
 // @ts-ignore
-import DeniReactTreeView from "deni-react-treeview";
+import DeniReactTreeView from 'deni-react-treeview'
 
-import { Renderer } from "./Renderer";
-import "react-reflex/styles.css";
-import { ReflexContainer, ReflexSplitter, ReflexElement } from "react-reflex";
-import { AddItemAndSubitemApi } from "../hooks/AddItem";
+import { Renderer } from './Renderer'
+import 'react-reflex/styles.css'
+import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex'
+import { AddItemAndSubitemApi } from '../hooks/AddItem'
 
-const MyContext = React.createContext({ selected: 0 });
+const MyContext = React.createContext({ selected: 0 })
 
 class Main extends React.Component<any, any> {
-  treeviewRef = React.createRef();
+  treeviewRef = React.createRef()
 
   constructor(props: any) {
-    super(props);
+    super(props)
     this.state = {
       treeNodes: [],
       selected: null,
-    };
+    }
 
-    this.setState({ selected: 100 });
+    this.setState({ selected: 100 })
   }
 
   onSelectItemHandler = (item: any) => {
-    this.setState({ selected: item.id });
-  };
+    this.setState({ selected: item.id })
+  }
 
   render() {
     return (
       <ReflexContainer orientation="vertical">
         <ReflexElement className="left-pane">
-          <div className="pane-content">
-            <DeniReactTreeView
-              ref={this.treeviewRef}
-              style={{ marginRight: "10px", marginBottom: "10px" }}
-              key={0}
-              showCheckbox={true}
-              theme={"classic"}
-              items={this.state.treeNodes}
-              onSelectItem={this.onSelectItemHandler}
-            />
-          </div>
+          <DeniReactTreeView
+            ref={this.treeviewRef}
+            style={{ marginRight: '10px', marginBottom: '10px' }}
+            key={0}
+            showCheckbox={true}
+            theme={'classic'}
+            items={this.state.treeNodes}
+            onSelectItem={this.onSelectItemHandler}
+          />
         </ReflexElement>
 
         <ReflexSplitter />
         <ReflexElement className="right-pane" minSize={800} maxSize={1600}>
-          <div className="pane-content">
-            <MyContext.Provider value={this.state.selected}>
-              <Renderer
-                selected={this.state.selected}
-                setSelected={(selected: number) => {
-                  this.setState({ selected: selected });
-                }}
-                setTree={(nodes: any) => {
-                  this.setState({ treeNodes: nodes });
-                }}
-              ></Renderer>
-            </MyContext.Provider>
-          </div>
+          <Renderer
+            selected={this.state.selected}
+            setSelected={(selected: number) => {
+              this.setState({ selected: selected })
+            }}
+            setTree={(nodes: any) => {
+              this.setState({ treeNodes: nodes })
+            }}
+          ></Renderer>
         </ReflexElement>
       </ReflexContainer>
-    );
+    )
   }
 }
 
-export { Main };
+export { Main }
