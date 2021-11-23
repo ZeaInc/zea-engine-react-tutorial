b5ba30780656e57413f33e333d1811359c5c9555
Seiji-zea Smith <seiji.smith@zea.live>
2021-11-23 14:21:38 -0800

    wip: tutorial 2 - layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Viewport3D } from "./Viewport3D";
-
+import 'react-reflex/styles.css'
+import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex'
 class Main extends React.Component<any, any> {
   constructor(props: any) {
     super(props)
@@ -11,7 +12,17 @@ class Main extends React.Component<any, any> {
 
   render() {
     return (
-      <Viewport3D></Viewport3D>
+      <ReflexContainer orientation="vertical">
+        <ReflexElement className="left-pane">
+          <label>Side bar</label>
+        </ReflexElement>
+
+        <ReflexSplitter />
+
+        <ReflexElement className="right-pane" flex={0.85}>
+          <Viewport3D/>
+        </ReflexElement>
+      </ReflexContainer>
     )
   }
 }
